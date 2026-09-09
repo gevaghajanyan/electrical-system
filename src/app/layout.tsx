5dce8e7b5c8a1df3ddc7f2c2649c08e47674ebd4
gevaghajanyan <g.aghajanyan1995@gmail.com>
2026-09-09 10:47:03 +0400

    feat: update project name to 'voltra' and adjust layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col bg-zinc-50 dark:bg-zinc-950 overscroll-none">
+      <body
+        className="min-h-full flex flex-col bg-zinc-50 dark:bg-zinc-950 overscroll-none"
+        suppressHydrationWarning
+      >
         <I18nProvider>
           <ThemeProvider>
             <InstallPromptProvider>
